3b310b44c6829802050c3067f31572e8bff53631
ChrisYuneva <christina_yuneva@mail.ru>
2023-11-30 13:25:02 +0400

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: '/Facite/',
     define: {
       'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
     },
